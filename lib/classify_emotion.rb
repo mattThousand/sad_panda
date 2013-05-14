c90953b8b5584bb619bcd3f0a2948c805595d1b3
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-05-13 21:54:51 -0400

    mv term_document_hash.rb status_message.rb

--- lib/classify_emotion.rb
+++ lib/classify_emotion.rb
@@ -1,4 +1,7 @@
 require "classify_emotion/version"
+require "classify_emotion/emotion"
+require "classify_emotion/emotions"
+require "classify_emotion/term_document_hash"
 
 module ClassifyEmotion
   # Your code goes here...
--- lib/classify_emotion/emotion.rb
+++ lib/classify_emotion/emotion.rb
@@ -1,10 +1,23 @@
-def classify_emotion message_text,algorithm="bayes",prior=1.0
-	term_document_hash = create_term_document_hash message_text
-	emotions = []
-	fo = File.open("emotions/emotions.csv","r")
-	fo.each_line do |l|
-		line = l.gsub("\r",",")
-		words = line.split(",")
-		words.each {|e| emotions << e}
+module classify_emotion
+	class Emotion
+		attr_reader :algorithm,:prior
+
+		def initialize algorithm="bayes",prior=1.0,term_document_hash
+			@term_document_hash = term_document_hash
+			@algorithm = algorithm
+			@prior = prior
+		end 
+
+		def classify_emotion message_text
+			term_document_hash = create_term_document_hash
+			emotions = []
+			fo = File.open("emotions/emotions.csv","r")
+			fo.each_line do |l|
+				line = l.gsub("\r",",")
+				words = line.split(",")
+				words.each {|e| emotions << e}
+			end
+		end
+
 	end
 end
