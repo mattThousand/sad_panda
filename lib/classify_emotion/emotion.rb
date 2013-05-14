0af6411c947de6e5b316a94da22177f0c17595c8
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-05-13 23:25:44 -0400

    modify classify_emotion/status_message.rb

--- lib/classify_emotion/emotion.rb
+++ lib/classify_emotion/emotion.rb
@@ -1,23 +1,5 @@
-module classify_emotion
-	class Emotion
-		attr_reader :algorithm,:prior
-
-		def initialize algorithm="bayes",prior=1.0,term_document_hash
-			@term_document_hash = term_document_hash
-			@algorithm = algorithm
-			@prior = prior
-		end 
-
-		def classify_emotion message_text
-			term_document_hash = create_term_document_hash
-			emotions = []
-			fo = File.open("emotions/emotions.csv","r")
-			fo.each_line do |l|
-				line = l.gsub("\r",",")
-				words = line.split(",")
-				words.each {|e| emotions << e}
-			end
-		end
+module ClassifyEmotion
+	class Emotion 
 
 	end
 end
