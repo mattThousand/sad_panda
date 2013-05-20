1c661266c4117e531049c7bab5ea2d798275b921
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-05-20 00:34:04 -0400

    Remove unnecessary tests, clean up rspec syntax
    Remove tests on third party libraries, remove redundant tests on
    methods

--- spec/status_message_spec.rb
+++ spec/status_message_spec.rb
@@ -4,121 +4,74 @@ module SadPanda
 	describe StatusMessage do
 
 		let(:status_message) {SadPanda::StatusMessage.new "a message"}
-		let(:emotions_hash) {status_message.get_term_emotion_hash}
-		let(:polarities_hash) {status_message.get_term_polarity_hash}
-		let(:term_frequency_hash) {status_message.create_term_frequency_hash_from_status_message}		
-
-		describe "has working methods" do
-			it "has an emotion method" do
-				expect(status_message.respond_to?(:emotion)).to eql(true)
-			end
-
-			it "has a polarity method" do
-				expect(status_message.respond_to?(:polarity)).to eql(true)
-			end
-
-			it "has a create_term_frequency_hash_from_status_message method" do
-				expect(status_message.respond_to?(:create_term_frequency_hash_from_status_message)).to eql(true)
-			end
-
-			it "has a get_emotion_score_from_words method" do
-				expect(status_message.respond_to?(:get_emotion_score_from_words)).to eql(true)
-			end
-
-			it "has a get_term_polarity_hash method" do
-				expect(status_message.respond_to?(:get_emotion_score_from_words)).to eql(true)
-			end
-
-			it "has a get_polarity_score_from_words method" do
-				expect(status_message.respond_to?(:get_emotion_score_from_words)).to eql(true)
-			end
-
-		end
+		let(:emotions) {status_message.get_term_emotions}
+		let(:polarities) {status_message.get_term_polarities}
+		let(:term_frequencies) {status_message.build_term_frequencies}
 
 		describe "when initialized" do
 			it "verbose defaults to false" do
-				expect(status_message.verbose).to eql(false)
+				expect(status_message.verbose).to be_false
 			end
 		end
 
-		describe "when 'create_term_frequency_hash_from_status_message' method is called" do
+		describe "when 'build_term_frequencies' method is called" do
 			context "when status_message is an empty string" do
 				it "returns an empty hash" do
 					empty_message = SadPanda::StatusMessage.new "   "
-					expect(empty_message.create_term_frequency_hash_from_status_message).to eql({})
+					expect(empty_message.build_term_frequencies).to be_empty
 				end
 			end
 
 			context "when status_message is a non-recogizable word" do
 				it "returns a empty hash with key == zorg and and value == 1" do
-					strange_message = SadPanda::StatusMessage.new "zorg"
-					expect(strange_message.create_term_frequency_hash_from_status_message).to eql({"zorg"=>1})
+					word = "zorg"
+					strange_message = SadPanda::StatusMessage.new word
+					expect(strange_message.build_term_frequencies).to eql({"zorg" => 1})
 				end
 			end
 
 			context "when status_message includes recognizable words" do
 				it "returns a non-empty hash" do
-					length = status_message.create_term_frequency_hash_from_status_message.length
-					expect(length).to be > 0 
-				end 
-			end
-		end
-
-		describe "when 'get_word_stems' method is called" do
-			context "when word is an empty string" do
-				it "returns an array of itself" do
-					expect(status_message.get_word_stems [' ']).to eql([' '])
-				end
-			end
-
-			context "when word is a non-recogizable word" do
-				it "returns an array containing itself" do
-					expect(status_message.get_word_stems ['zorg']).to eql(['zorg'])
-				end
-			end
-
-			context "when word is a recognizable word" do
-				it "returns a string" do
-					stem = status_message.get_word_stems ['purple']
-					expect(stem[0].class).to eql(String)
+					hash = status_message.build_term_frequencies
+					expect(hash).to_not be_empty
 				end
 			end
 		end
 
-		describe "when 'get_term_emotion_hash method' is called" do
+		describe "when 'get_term_emotions method' is called" do
 			it 'returns a hash' do
-				output = status_message.get_term_emotion_hash
+				output = status_message.get_term_emotions
 				expect(output.class).to eql(Hash)
 			end
 
-			it "is non-empty" do 
-				output = status_message.get_term_emotion_hash
-				expect(output.length).to be > 0
+			it "is non-empty" do
+				output = status_message.get_term_emotions
+				expect(output).to_not be_empty
 			end
 		end
 
-		describe "when 'get_term_polarity_hash' method is called" do
+		describe "when 'get_term_polarities' method is called" do
 			it "returns a hash" do
-				output = status_message.get_term_polarity_hash
+				output = status_message.get_term_polarities
 				expect(output.class).to eql(Hash)
 			end
 
 			it "is non empty" do
-				output = status_message.get_term_polarity_hash
-				expect(output.length).to be > 0
+				output = status_message.get_term_polarities
+				expect(output).to_not be_empty
 			end
 		end
 
-		describe "when 'get_emotion_score_from_words' method is called" do
+		describe "when 'get_emotion_score' method is called" do
 			it 'returns a string' do
-				output = status_message.get_emotion_score_from_words emotions_hash,term_frequency_hash
+				output = status_message.get_emotion_score emotions,term_frequencies
 				expect(output.class).to eql(String)
 			end
 		end
 
-		describe "when 'get_polarity_score_from_words' method is called" do
+		describe "when 'get_polarity_score' method is called" do
 			it 'returns a string' do
-				output = status_message.get_polarity_score_from_words polarities_hash,term_frequency_hash
+				output = status_message.get_polarity_score polarities,term_frequencies
 				expect(output.class).to eql(Fixnum)
 			end
 		end
@@ -179,7 +132,7 @@ module SadPanda
 				end
 			end
 
-			context "when status_message == '  ' " do 
+			context "when status_message == '  ' " do
 				it "emotion is 'Uncertain'" do
 					status_message = SadPanda::StatusMessage.new "  "
 				end
@@ -197,7 +150,7 @@ module SadPanda
 				it "polarity is greater than zero" do
 					status_message = SadPanda::StatusMessage.new "I am happy"
 					expect(status_message.polarity).to be > 0
-				end 
+				end
 			end
 
 
@@ -243,7 +196,7 @@ module SadPanda
 				end
 			end
 
-			context "when status_message == '  ' " do 
+			context "when status_message == '  ' " do
 				it "polarity is zero" do
 					status_message = SadPanda::StatusMessage.new "  "
 					expect(status_message.polarity).to eql(0)
