e5a900fbcc7cc799aee9f937132ea0c2a7402d79
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-06-06 21:55:39 -0400

    Fix bug in sad_panda.rb

--- lib/sad_panda.rb
+++ lib/sad_panda.rb
@@ -1,5 +1,4 @@
 require_relative "./sad_panda/version"
-require_relative "./sad_panda/status_message"
 require_relative './sad_panda/emotions/emotion_bank.rb'
 require_relative './sad_panda/emotions/term_polarities.rb'
 require_relative './sad_panda/emotions/stopwords.rb'
