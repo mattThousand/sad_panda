9e51b15d68af2e86f23c275347eb62e10e10aa41
Edwin Rozario <edwin@boost.co.nz>
2017-09-19 19:28:50 +1200

    commenting methods in polarity

--- lib/sad_panda/polarity.rb
+++ lib/sad_panda/polarity.rb
@@ -12,6 +12,7 @@ module SadPanda
       @polarities = []
     end
 
+    # Main method that initiates calculating polarity
     def call
       words = stem_words(remove_stopwords(@words))
 
@@ -22,24 +23,28 @@ module SadPanda
 
     private
 
+    # Checks if words has a happy emoji
     def happy_emoticon?
       words.include?(':)') ||
         words.include?(':-)') ||
         words.include?(':]') || words.include?(':-]')
     end
 
+    # Checks if words has a sad emoji
     def sad_emoticon?
       words.include?(':(') ||
         words.include?(':-(') ||
         words.include?(':[') || words.include?(':-[')
     end
 
+    # Checks if words has happy or sad emoji and adds polarity for it
     def score_emoticon_polarity
       polarities << 5.0 if happy_emoticon? && sad_emoticon?
       polarities << 8.0 if happy_emoticon?
       polarities << 2.0 if sad_emoticon?
     end
 
+    # Appends polarities of words to array polarities
     def score_polarities_for(word_frequencies)
       word_frequencies.each do |word, frequency|
         polarity = SadPanda::Polarities[word.to_sym]
